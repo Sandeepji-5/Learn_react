0f05e1c6adf0b5a3f8ddf0754b747bb13984fe15
Sandeep Vishwakarma <121304075+Sandeepji-5@users.noreply.github.com>
2025-02-14 10:30:08 +0000

    changes color

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -5,8 +5,8 @@ import './App.css'
 
 function App() {
 
-  let  [counter, setCounter] = useState(106);
-  let [rounds, curRounds]= useState(16);
+  let  [counter, setCounter] = useState(1);
+  let [rounds, curRounds]= useState(1);
   //let   counter = 15;
   const addValue = ()=>{
 
@@ -36,18 +36,19 @@ const removeValue = ()=>{
 }
   return (
     <>
-     
-    <h2 >"HARE KRISHNA HARE KRISHNA KRISHNA KRISHNA HARE HARE </h2>
+     <div class= 'jai'> 
+     <h2 >"HARE KRISHNA HARE KRISHNA KRISHNA KRISHNA HARE HARE </h2>
     <h2>   HARE RAMA HARE RAMA RAMA RAMA HARE HARE !!" </h2>
-
+ </div>
+    
      
       <h1>Counter value : {counter}</h1>
 
 
       <button onClick={addValue} >Add Value</button>
       <br/><br/>
-      <button onClick={removeValue}>Remove Value</button>
-      <p>Completed Rounds :{rounds}</p>
+      <button onClick={removeValue}> Reduce Value</button>
+      <p> <strong>Your Rounds :{rounds}</strong></p>
       
     </>
   )
